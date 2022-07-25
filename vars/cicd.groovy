24c9437220ba22b213e4a0ea7dadabd01fc0f746
noor739 <103039347+noor739@users.noreply.github.com>
2022-07-25 17:03:57 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -16,7 +16,7 @@ def newTest(jobname)
 }
 def newDelivery(ip,appname)
 {
-   deploy adapters: [tomcat9(credentialsId: 'bdc997f0-20fd-4058-a0b6-f5b404b678ec', path: '', url: "${ip}")], contextPath: "${appname}", war: '**/*.war'
+   deploy adapters: [tomcat9(credentialsId: 'ea02b6de-cb1b-4b09-a6e1-e199550877e5', path: '', url: "${ip}")], contextPath: "${appname}", war: '**/*.war'
 }
   
 
